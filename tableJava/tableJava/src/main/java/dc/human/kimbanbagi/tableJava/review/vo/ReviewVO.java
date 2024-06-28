9e6a095fa60a1a572d703b2b16d7de17d6ef4b98
박지민 <55971771+MeGuuuun@users.noreply.github.com>
2024-06-28 17:13:40 +0900

    0628 commit

--- tableJava/tableJava/src/main/java/dc/human/kimbanbagi/tableJava/review/vo/ReviewVO.java
+++ tableJava/tableJava/src/main/java/dc/human/kimbanbagi/tableJava/review/vo/ReviewVO.java
@@ -5,13 +5,47 @@ import java.sql.Date;
 public class ReviewVO {
     String rId;
     String rName;
-    String rAddress;
+    String rDate;
+    String rTime;
     String uId;
-    String star;
+    String uName;
+    int star;
     String grade;
     String review;
     Date createdDate;
     Date updatedDate;
+
+    public String getrDate() {
+        return rDate;
+    }
+
+    public void setrDate(String rDate) {
+        this.rDate = rDate;
+    }
+
+    public String getrTime() {
+        return rTime;
+    }
+
+    public void setrTime(String rTime) {
+        this.rTime = rTime;
+    }
+
+    public String getuName() {
+        return uName;
+    }
+
+    public void setuName(String uName) {
+        this.uName = uName;
+    }
+
+    public int getStar() {
+        return star;
+    }
+
+    public void setStar(int star) {
+        this.star = star;
+    }
 
     public String getrId() {
         return rId;
@@ -27,22 +61,6 @@ public class ReviewVO {
 
     public void setrName(String rName) {
         this.rName = rName;
-    }
-
-    public String getrAddress() {
-        return rAddress;
-    }
-
-    public void setrAddress(String rAddress) {
-        this.rAddress = rAddress;
-    }
-
-    public String getStar() {
-        return star;
-    }
-
-    public void setStar(String star) {
-        this.star = star;
     }
 
     public String getuId() {
